cd37f1b4ead420a900afa3a514a575f761a394ef
Biw_CS. <Piyawatbiwkai@gmail.com>
2026-06-23 22:23:50 +0700

    Update knacksat_orbit.c
    Signed-off-by: Biw_CS. <Piyawatbiwkai@gmail.com>

--- Basic skills 69/Basic Skills 69/knacksat_orbit.c
+++ Basic skills 69/Basic Skills 69/knacksat_orbit.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
  int main(){
-    int tall = 0;
-    scanf("%d", &tall);
+    float tall = 0.0;
+    scanf("%f", &tall);
 
     if(tall <= 0){
         printf("Error: Invalid Altitude");
     }
-    else if(tall >= 1 && tall < 400){
-        printf("In progress: %.1lf km remaining", (double)(400.0 - tall));
+    else if(tall >= 0.1 && tall < 400){
+        printf("In progress: %.1lf km remaining", (400.0 - tall));
     }
     else if(tall == 400){
         printf("KnackSat-2 has reached orbit!");
     }
     else if(tall > 400){
-        printf("Out of orbit: %.1lf km over", (double)(tall - 400.0));
+        printf("Out of orbit: %.1lf km over", (tall - 400.0));
     }
 
     return 0;
